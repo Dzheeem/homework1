516fad970ec137b38fe0aee8989c1d915e5199d8
Dzheeem <Dzheeem@yandex.ru>
2024-08-12 01:35:45 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string=input("Я считаю символы, введите что нибудь: ")
+print('В вашем тексте' , len(my_string) , 'символа(ов)' )
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
